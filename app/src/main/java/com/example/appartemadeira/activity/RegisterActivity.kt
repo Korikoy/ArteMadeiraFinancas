fdcea63f45b71f354483814ec8ceee6605080091
Korikoy <mauriciomatheus556@hotmail.com>
2022-09-08 16:24:33 -0300

    Dropdonw box completa para layout de registro de conta, layout de registro completo(sujeito de alteraçoes), adicionado firebase Firestore ao projeto, luxo de "conta" para armezenar no firebase completo.
    Iniciando recycleview e recuperaçao de dados do firebase, para exibir lintas de contas pendentes e pagas.

--- app/src/main/java/com/example/appartemadeira/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/appartemadeira/activity/RegisterActivity.kt
@@ -1,31 +1,18 @@
 package com.example.appartemadeira.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.appartemadeira.R
 import com.example.appartemadeira.databinding.ActivityResisterBinding
+import com.example.appartemadeira.model.Bills
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResisterBinding
     private lateinit var toolbar: Toolbar
 
-
-    override fun onResume() {
-        super.onResume()
-        val categoryBillsToPay = resources.getStringArray(R.array.categoryBillsToPay)
-        val categoryBillsToRec = resources.getStringArray(R.array.categoryBillsToRecive)
-        if(binding.radioButtonBillsToPay.isChecked){
-            val arrayAdapter = ArrayAdapter(this,R.layout.dropdonw_item,categoryBillsToPay)
-            binding.autoCompleteText.setAdapter(arrayAdapter)
-        }else if(binding.radioButtonBillsToRecive.isChecked){
-            val arrayAdapter = ArrayAdapter(this,R.layout.dropdonw_item,categoryBillsToRec)
-            binding.autoCompleteText.setAdapter(arrayAdapter)
-        }
-
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResisterBinding.inflate(layoutInflater)
@@ -33,7 +20,55 @@ class RegisterActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         setContentView(binding.root)
 
+
+
+
+        binding.radioButtonBillsToPay.setOnClickListener {
+            dropBoxBillsToPay()
+        }
+        binding.radioButtonBillsToRecive.setOnClickListener {
+            dropBoxBillsToRecive()
+        }
+        binding.buttonAdd.setOnClickListener {
+            registerBills()
+        }
+
+
+
     }
+    private fun registerBills(){
+        if(validadeFilds()) {
+            val name = binding.editTextName.text.toString()
+            val category = binding.autoCompleteText.text.toString()
+            val type = if (binding.radioButtonBillsToRecive.isChecked) {
+                "Contas a receber"
+            } else {
+                "Contas a pagar"
+            }
+            val valor = binding.editTextCash.text.toString()
+            val description: String? = binding.editTextDescrip.text?.toString()
+            val bills = Bills(name, type,category,valor,description)
+            bills.save()
+            Toast.makeText(this,"Sucesso ao cadastrar conta", Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText(this,"Falha ao cadastrar conta, preencha os dados", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun dropBoxBillsToPay(){
+        val categoryBillsToPay = resources.getStringArray(R.array.categoryBillsToPay)
+        val arrayAdapter = ArrayAdapter(this,R.layout.dropdonw_item,categoryBillsToPay)
+        binding.textInputLayoutCategory.isEnabled = true
+        binding.autoCompleteText.setAdapter(arrayAdapter)
+    }
+    private fun dropBoxBillsToRecive(){
+        val categoryBillsToRec = resources.getStringArray(R.array.categoryBillsToRecive)
+        val arrayAdapter = ArrayAdapter(this,R.layout.dropdonw_item,categoryBillsToRec)
+        binding.textInputLayoutCategory.isEnabled = true
+        binding.autoCompleteText.setAdapter(arrayAdapter)
+    }
+
     private fun toolbarConfiguration(){
         toolbar = binding.include.toolbarPrincipal
         toolbar.title = "Registrar Conta"
@@ -41,4 +76,18 @@ class RegisterActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close_black_24)
     }
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return false
+    }
+    private fun validadeFilds():Boolean{
+        if (binding.editTextName.text != null && binding.editTextCash.text != null && binding.autoCompleteText.text != null){
+            return true
+        }
+        return false
+    }
+
+
 }
+
+
